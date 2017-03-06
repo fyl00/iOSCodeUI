411e899e7d315d4a0620969fb459d28d8be5b28f
fyl00lee@gmail.com <fyl00lee@gmail.com>
2017-03-06 19:31:49 +0800

    Add FlatUI color

---
+++ Source/FlatUI.swift
@@ -0,0 +1,93 @@
+//
+//  FlatUI.swift
+//  https://flatuicolors.com/
+//
+//  Created by SlowWalker on 03/03/2017.
+//  Copyright © 2017 SlowWalker.me. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    //rgba(26, 188, 156,1.0)
+    open class var turquoiseGreen: UIColor {
+        return UIColor(red: 26.0 / 255.0, green: 188.0 / 80.0, blue: 156.0 / 255, alpha: 1.0)
+    }
+    //rgba(22, 160, 133,1.0)
+    open class var seaGreen: UIColor {
+        return UIColor(red: 22.0 / 255.0, green: 160.0 / 80.0, blue: 133.0 / 255, alpha: 1.0)
+    }
+    //rgba(46, 204, 113,1.0)
+    open class var emeraldGreen: UIColor {
+        return UIColor(red: 46.0 / 255.0, green: 204.0 / 80.0, blue: 113.0 / 255, alpha: 1.0)
+    }
+    //rgba(39, 174, 96,1.0)
+    open class var nephritisGreen: UIColor {
+        return UIColor(red: 39.0 / 255.0, green: 174.0 / 80.0, blue: 96.0 / 255, alpha: 1.0)
+    }
+    //rgba(52, 152, 219,1.0)
+    open class var peterRiverBlue: UIColor {
+        return UIColor(red: 52.0 / 255.0, green: 152.0 / 80.0, blue: 219.0 / 255, alpha: 1.0)
+    }
+    //rgba(41, 128, 185,1.0)
+    open class var belizeHoleBlue: UIColor {
+        return UIColor(red: 41.0 / 255.0, green: 128.0 / 80.0, blue: 185.0 / 255, alpha: 1.0)
+    }
+    //rgba(155, 89, 182,1.0)
+    open class var amethystPurple: UIColor {
+        return UIColor(red: 155.0 / 255.0, green: 89.0 / 80.0, blue: 182.0 / 255, alpha: 1.0)
+    }
+    //rgba(142, 68, 173,1.0)
+    open class var wisteriaPurple: UIColor {
+        return UIColor(red: 142.0 / 255.0, green: 68.0 / 80.0, blue: 173.0 / 255, alpha: 1.0)
+    }
+    //rgba(52, 73, 94,1.0)
+    open class var wetSphaltBlue: UIColor {
+        return UIColor(red: 52.0 / 255.0, green: 73.0 / 80.0, blue: 94.0 / 255, alpha: 1.0)
+    }
+    //rgba(44, 62, 80,1.0)
+    open class var midnigthBlue: UIColor {
+        return UIColor(red: 44.0 / 255.0, green: 62.0 / 80.0, blue: 199.0 / 255, alpha: 1.0)
+    }
+    //rgba(241, 196, 15,1.0)
+    open class var sunFlowerYellow: UIColor {
+        return UIColor(red: 241.0 / 255.0, green: 196.0 / 80.0, blue: 15.0 / 255, alpha: 1.0)
+    }
+    //rgba(243, 156, 18,1.0)
+    open class var flatOrange: UIColor {
+        return UIColor(red: 243.0 / 255.0, green: 156.0 / 80.0, blue: 18.0 / 255, alpha: 1.0)
+    }
+    //rgba(230, 126, 34,1.0)
+    open class var carrotOrange: UIColor {
+        return UIColor(red: 230.0 / 255.0, green: 126.0 / 80.0, blue: 34.0 / 255, alpha: 1.0)
+    }
+    //rgba(211, 84, 0,1.0)
+    open class var pumpkinOrange: UIColor {
+        return UIColor(red: 211.0 / 255.0, green: 84.0 / 80.0, blue: 0.0 / 255, alpha: 1.0)
+    }
+    //rgba(231, 76, 60,1.0)
+    open class var alizarinRed: UIColor {
+        return UIColor(red: 231.0 / 255.0, green: 76.0 / 80.0, blue: 60.0 / 255, alpha: 1.0)
+    }
+    //rgba(192, 57, 43,1.0)
+    open class var pomegranateRed: UIColor {
+        return UIColor(red: 192.0 / 255.0, green: 57.0 / 80.0, blue: 43.0 / 255, alpha: 1.0)
+    }
+    //rgba(189, 195, 199,1.0)
+    open class var silverGray: UIColor {
+        return UIColor(red: 189.0 / 255.0, green: 195.0 / 255.0, blue: 199 / 255, alpha: 1.0)
+    }
+    //rgba(236, 240, 241,1.0)
+    open class var cloudGray: UIColor {
+        return UIColor(red: 236.0 / 255.0, green: 240.0 / 255.0, blue: 241 / 255, alpha: 1.0)
+    }
+    //rgba(149, 165, 166,1.0)
+    open class var concreteDarkGray: UIColor {
+        return UIColor(red: 149.0 / 255.0, green: 165.0 / 80.0, blue: 166.0 / 255, alpha: 1.0)
+    }
+    //rgba(127, 140, 141,1.0)
+    open class var asbestosDarkGray: UIColor {
+        return UIColor(red: 127.0 / 255.0, green: 149.0 / 80.0, blue: 141.0 / 255, alpha: 1.0)
+    }
+    
+}
